4537a96b4bc0690950ab1d476d25cb989ff7415b
Edwin Marroquin <efmarroquinb@correo.udistrital.edu.co>
2022-03-27 22:00:23 -0500

    Revert "Estacion meteorologica"
    This reverts commit 778ed98c46cb130c865d1ad2dc6d6eb1acab150f.

--- src/main/src/iotDHT/iotDHT.h
+++
@@ -1,3 +0,0 @@
-#include <DHT.h> // Libreria DHT
-
-DHT dht(DHTPIN, DHTTYPE); // Instaciacion de la clase para el Sensor DHT
--- src/main/src/iotWIFI/iotWIFI.h
+++
@@ -1,4 +0,0 @@
-#include <DNSServer.h>
-#include <WiFiManager.h>
-
-WiFiManager wm;
